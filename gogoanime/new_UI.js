2078d4d1741c9565c849b381fadc9482b827f747
Sharad Raj <dev_sharad@outlook.com>
2021-12-05 12:08:06 +0530

    v 0.2.0

--- gogoanime/new_UI.js
+++ gogoanime/new_UI.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Gogoanime New UI
-// @version      0.1.0
+// @version      0.2.0
 // @namespace    https://sharadcodes.github.io
 // @author       sharadcodes
 // @license      MIT
@@ -121,7 +121,6 @@
       })
     })
   }
-  
   
   function tagDubbed() {
     document.querySelectorAll('.items li').forEach(e=>{
