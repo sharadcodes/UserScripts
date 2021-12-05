a939460f458e93184f865fa9b1def4430001eade
Sharad Raj <dev_sharad@outlook.com>
2021-12-05 12:18:20 +0530

    Update and rename new_UI.js to new_UI_all_in_one.js

--- gogoanime/new_UI_all_in_one.js
+++ gogoanime/new_UI_all_in_one.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Gogoanime New UI
-// @version      0.2.0
+// @version      1.0.0
 // @namespace    https://sharadcodes.github.io
 // @author       sharadcodes
 // @license      MIT
